016fcf03e372ee2dd097192645fd9ef5e272089f
Andres Lopez <andres.lp.dev@gmail.com>
2024-07-23 16:55:12 -0500

    test subnets group for rds

--- modules/rds_module/01.subnet_group.tf
+++ modules/rds_module/01.subnet_group.tf
@@ -2,8 +2,8 @@ resource "aws_db_subnet_group" "private_subnet_group" {
   name        = "${var.db_identifier}-private-subnet-group"
   description = "Private subnet group for RDS"
   subnet_ids  = [
-    var.subnet_ids["us-east-1a"].id,
-    var.subnet_ids["us-east-1b"].id,
+    var.subnet_ids["us-east-1a"],
+    var.subnet_ids["us-east-1b"],
   ]
 
   tags = {
