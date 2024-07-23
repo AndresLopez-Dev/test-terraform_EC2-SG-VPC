b9675c1644eb20842641cd122f39f5f81aa4f2ed
Andres Lopez <andres.lp.dev@gmail.com>
2024-07-23 14:54:27 -0500

    modified private subnets cidr

--- modules/vpc_module/00.variables.tf
+++ modules/vpc_module/00.variables.tf
@@ -32,8 +32,8 @@ variable "pub_subnets_cidr" {
 variable "priv_subnets_cidr" {
   type = map(string)
   default = {
-    "us-east-1a" = "10.0.1.0/24"
-    "us-east-1b" = "10.0.2.0/24"
+    "us-east-1a" = "172.31.20.0/24"
+    "us-east-1b" = "172.31.21.0/24"
   }
   description = "Subredes privadas"
 }
