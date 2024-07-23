76e937e24721d6a3bf3f3bd6b3bd2592427e7073
Andres Lopez <andres.lp.dev@gmail.com>
2024-07-23 15:32:18 -0500

    modified private subnets cidr

--- modules/vpc_module/20.internet_gateway.tf
+++ modules/vpc_module/20.internet_gateway.tf
@@ -3,7 +3,7 @@ resource "aws_internet_gateway" "internet_gw" {
   vpc_id = aws_vpc.test_vpc.id
 
   tags = {
-    Name = "${var.vpc_name}-int-gw"
+    Name = "${var.vpc_name}-igw"
   }
   depends_on = [aws_vpc.test_vpc]
 }
